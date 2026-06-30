b6c50fdb622a1e62786935b5e51881369b8717f1
Marchell <imanuel.marchell@gmail.com>
2026-06-30 17:36:06 +0800

    format

--- crates/medley-core/src/index/tantivy_text.rs
+++ crates/medley-core/src/index/tantivy_text.rs
@@ -165,7 +165,9 @@ fn run_search(
     let top_docs = searcher
         .search(
             &search_query,
-            &TopDocs::with_limit(MAX_SEARCH_HITS).and_offset(0).order_by_score(),
+            &TopDocs::with_limit(MAX_SEARCH_HITS)
+                .and_offset(0)
+                .order_by_score(),
         )
         .map_err(map_tantivy_err)?;
 
